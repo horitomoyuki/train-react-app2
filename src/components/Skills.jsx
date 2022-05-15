47f20dc253564f373ea15fd7e82b4bd26dd4a624
horitomoyuki <eziltom0131@gmail.com>
2022-05-15 21:07:59 +0900

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useReducer } from 'react';
 import axios from 'axios';
 import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
+import { requestStates } from '../constants';
 
 export const Skills = () => {
   const [state, dispatch] = useReducer(skillReducer, initialState);
@@ -21,7 +22,7 @@ export const Skills = () => {
   const generateLanguageCountObj = (allLanguageList) => {
     const notNullLanguageList = allLanguageList.filter(language => language != null);
     const uniqueLanguageList = [...new Set(notNullLanguageList)];
- 
+
     return uniqueLanguageList.map(item => {
       return {
         language: item,
@@ -37,6 +38,16 @@ export const Skills = () => {
           <h2>Skills</h2>
         </div>
         <div className="skills-container">
+          {
+            state.requestState === requestStates.loading && (
+              <p className="description">取得中...</p>
+            )
+          }
+          {
+            state.requestState === requestStates.error && (
+              <p className="description">エラーが発生しました</p>
+            )
+          }
         </div>
       </div>
     </div>
