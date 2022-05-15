8c1f1fe52c15fe22a8fc9d00ed75c3de65e57d3b
horitomoyuki <eziltom0131@gmail.com>
2022-05-15 15:31:20 +0900

    Skillコンポーネント追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 // Headerコンポーネントをimportする
 import { Header } from './components/Header';
+import { Skills } from './components/Skills';
 
 function App() {
   return (
-    <Header /> // Headerコンポーネントを呼び出す
+    <>
+      <Header />
+      <Skills />
+    </>
   );
 }
 
