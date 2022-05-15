d0c5e46c11f16bab460d73e432255fd5ced142e3
horitomoyuki <eziltom0131@gmail.com>
2022-05-15 17:46:22 +0900

    Create skillReducer.js

---
+++ src/reducers/skillReducer.js
@@ -0,0 +1,31 @@
+import { requestStates } from '../constants';
+
+export const actionTypes = {
+  initial: 'INITIAL',
+  fetch: 'FETCHING',
+  success: 'FETCH_SUCCESS',
+  error: 'FETCH_ERROR'
+};
+
+export const initialState = {
+  languageList: [],
+  requestState: requestStates.idle,
+}
+
+export const skillReducer = (
+  state, action
+) => {
+  switch(action.type) {
+    case actionTypes.initial: {
+    }
+    case actionTypes.fetch: {
+    }
+    case actionTypes.success: {
+    }
+    case actionTypes.error: {
+    }
+    default: {
+      throw new Error();
+    }
+  }
+};
