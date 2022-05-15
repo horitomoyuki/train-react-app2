f46aacafac26c2b15d9860a83025081bd71ee47c
horitomoyuki <eziltom0131@gmail.com>
2022-05-15 15:35:45 +0900

    Contactコンポーネント追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-// Headerコンポーネントをimportする
 import { Header } from './components/Header';
 import { Skills } from './components/Skills';
+import { Contact } from './components/Contact';
 
 function App() {
   return (
     <>
       <Header />
       <Skills />
+      <Contact />
     </>
   );
 }
